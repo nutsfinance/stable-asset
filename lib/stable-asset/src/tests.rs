be3efe87635df16340a336f22ad08e9bc79de7be
Shengda Ding <dingshengda@gmail.com>
2022-06-22 22:43:54 -0700

    Fix overflow

--- lib/stable-asset/src/tests.rs
+++ lib/stable-asset/src/tests.rs
@@ -381,12 +381,11 @@ fn mint_failed_overflow() {
 		let pool_tokens = create_pool();
 		System::set_block_number(2);
 		match pool_tokens {
-			(_coin0, _coin1, _pool_asset, _swap_id) => {
+			(coin0, coin1, _pool_asset, _swap_id) => {
+				assert_ok!(TestAssets::mint_into(coin0, &1, 10000000000u128));
+				assert_ok!(TestAssets::mint_into(coin1, &1, 20000000000u128));
 				let amounts = vec![10000000000u128, 20000000000u128];
-				assert_noop!(
-					StableAsset::mint(Origin::signed(1), 0, amounts, 0u128),
-					Error::<Test>::Math
-				);
+				assert_ok!(StableAsset::mint(Origin::signed(1), 0, amounts, 0u128),);
 			}
 		}
 	});

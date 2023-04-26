4dc930acabf28b95a8ff6dc2b58cfa22cb05327f
Frank Yin <ukby.1234@gmail.com>
2023-04-25 19:48:29 -0700

    new calculation method

--- lib/stable-asset/src/lib.rs
+++ lib/stable-asset/src/lib.rs
@@ -1235,11 +1235,8 @@ impl<T: Config> Pallet<T> {
 				.ok_or(Error::<T>::Math)?;
 		}
 		let new_d: T::AtLeast64BitUnsigned = Self::get_d(&balances, a).ok_or(Error::<T>::Math)?;
-		let new_d_margin: T::AtLeast64BitUnsigned = new_d
-			.checked_mul(&99u128.into())
-			.ok_or(Error::<T>::Math)?
-			.checked_div(&100u128.into())
-			.ok_or(Error::<T>::Math)?;
+		let new_d_div_100: T::AtLeast64BitUnsigned = new_d.checked_div(&100u128.into()).ok_or(Error::<T>::Math)?;
+		let new_d_margin: T::AtLeast64BitUnsigned = new_d.checked_sub(&new_d_div_100).ok_or(Error::<T>::Math)?;
 
 		if new_d > old_d {
 			let fee_amount: T::AtLeast64BitUnsigned = new_d.checked_sub(&old_d).ok_or(Error::<T>::Math)?;

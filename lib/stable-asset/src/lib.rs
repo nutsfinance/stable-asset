0505e4f73ac387d3f82db2bdcc0893c70b80713f
Frank Yin <ukby.1234@gmail.com>
2023-04-25 19:48:53 -0700

    add checks in fee collection validation (#40)
    * add checks in fee collection validation
    
    * new calculation method

--- lib/stable-asset/src/lib.rs
+++ lib/stable-asset/src/lib.rs
@@ -1235,6 +1235,8 @@ impl<T: Config> Pallet<T> {
 				.ok_or(Error::<T>::Math)?;
 		}
 		let new_d: T::AtLeast64BitUnsigned = Self::get_d(&balances, a).ok_or(Error::<T>::Math)?;
+		let new_d_div_100: T::AtLeast64BitUnsigned = new_d.checked_div(&100u128.into()).ok_or(Error::<T>::Math)?;
+		let new_d_margin: T::AtLeast64BitUnsigned = new_d.checked_sub(&new_d_div_100).ok_or(Error::<T>::Math)?;
 
 		if new_d > old_d {
 			let fee_amount: T::AtLeast64BitUnsigned = new_d.checked_sub(&old_d).ok_or(Error::<T>::Math)?;
@@ -1243,13 +1245,15 @@ impl<T: Config> Pallet<T> {
 				balances: Self::convert_vec_number_to_balance(balances),
 				total_supply: new_d.into(),
 			})
-		} else {
+		} else if old_d >= new_d_margin {
 			// this is due to rounding issues for token balance conversion
 			Ok(PendingFeeResult {
 				fee_amount: Zero::zero(),
 				balances: Self::convert_vec_number_to_balance(balances),
 				total_supply: new_d.into(),
 			})
+		} else {
+			Err(Error::<T>::Math)
 		}
 	}
 

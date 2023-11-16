4577e7178846c03ef0631dd2561d2a5bea128df6
Bryan Chen <xlchen1291@gmail.com>
2023-11-16 16:15:29 +1300

    update for 1.3.0

--- lib/stable-asset/src/mock.rs
+++ lib/stable-asset/src/mock.rs
@@ -81,7 +81,8 @@ impl pallet_balances::Config for Test {
 	type WeightInfo = ();
 	type MaxReserves = ();
 	type ReserveIdentifier = ();
-	type RuntimeHoldReason = ();
+	type RuntimeHoldReason = RuntimeHoldReason;
+	type RuntimeFreezeReason = RuntimeFreezeReason;
 	type FreezeIdentifier = ();
 	type MaxHolds = ();
 	type MaxFreezes = ();
